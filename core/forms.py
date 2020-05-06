b4a46e93eecea515fcab0fe8c85e68ce90fdd12a
Your Name <you@example.com>
2020-05-06 15:43:18 +0530

    first

---
+++ core/forms.py
@@ -0,0 +1,65 @@
+from django import forms
+from django_countries.fields import CountryField
+from django_countries.widgets import CountrySelectWidget
+
+
+PAYMENT_CHOICES = (
+    ('S', 'Stripe'),
+    ('D', 'Cash on Delivery')
+)
+AREA_CHOICES = (
+    ('BTM', 'BIRTAMOD'),
+    ('DMK', 'DAMAK'),
+    ('BTB', 'BIRTABAZAR'),
+    ('SNC', 'SANISCHARE' )
+)
+
+
+class CheckoutForm(forms.Form):
+    phone_number = forms.CharField(required=False)
+    shipping_address = forms.CharField(required=False)
+    shipping_address2 = forms.CharField(required=False)
+    shipping_country = CountryField(blank_label='(select country)').formfield(
+        required=False,
+        widget=CountrySelectWidget(attrs={
+            'class': 'custom-select d-block w-100',
+        }))
+
+    billing_address = forms.CharField(required=False)
+    billing_address2 = forms.CharField(required=False)
+    billing_country = forms.ChoiceField(
+        choices=AREA_CHOICES,
+        widget=forms.Select(attrs={'class':'browser-default custom-select'})
+    )
+
+    same_billing_address = forms.BooleanField(required=False)
+    set_default_shipping = forms.BooleanField(required=False)
+    use_default_shipping = forms.BooleanField(required=False)
+    set_default_billing = forms.BooleanField(required=False)
+    use_default_billing = forms.BooleanField(required=False)
+
+    payment_option = forms.ChoiceField(
+        widget=forms.RadioSelect, choices=PAYMENT_CHOICES)
+
+
+class CouponForm(forms.Form):
+    code = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Promo code',
+        'aria-label': 'Recipient\'s username',
+        'aria-describedby': 'basic-addon2'
+    }))
+
+
+class RefundForm(forms.Form):
+    ref_code = forms.CharField()
+    message = forms.CharField(widget=forms.Textarea(attrs={
+        'rows': 4
+    }))
+    email = forms.EmailField()
+
+
+class PaymentForm(forms.Form):
+    stripeToken = forms.CharField(required=False)
+    save = forms.BooleanField(required=False)
+    use_default = forms.BooleanField(required=False)
